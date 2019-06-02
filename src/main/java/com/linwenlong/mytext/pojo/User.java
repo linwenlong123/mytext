4b08e03b5a848e36dd8f5b6d39f9ee2edff03434
287202931@qq.com <Lwl666666>
2019-06-02 23:19:57 +0800

    第一次提交

---
+++ src/main/java/com/linwenlong/mytext/pojo/User.java
@@ -0,0 +1,13 @@
+package com.linwenlong.mytext.pojo;
+
+public class User {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
